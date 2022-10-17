90cb1f035f2c7debca8d6697b7a742ddff78a1a2
Czekaj Mateusz <mateusz.czek332@gmail.com>
2022-10-17 20:36:40 +0200

    Init
    Instalation and configuration modules

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+    watch: true,
+    entry: './src/index.ts',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        publicPath: 'public',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    }
+}
